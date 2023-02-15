28bd82e04474c572ff8f77f0bb0fde5d51a6e72a
naopandao <nao_yoshida@yunixy.co.jp>
2023-02-15 16:12:04 +0900

    routeに/usersを追加した

--- src/main.rs
+++ src/main.rs
@@ -16,7 +16,8 @@ async fn main() {
     tracing_subscriber::fmt::init();
 
     let app = Router::new()
-        .route("/", get(root));
+        .route("/", get(root))
+        .route("/users", post(create_user));
     let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
     tracing::debug!("listening on {}", addr);
     axum::Server::bind(&addr)
